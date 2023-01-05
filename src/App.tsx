0f1033c5505bb8ac318913018a6d3a54aa7727ab
Jose Carlos Filho <iamjose.filho@gmail.com>
2023-01-04 22:11:44 -0400

    feat: 1st version

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Header } from './components/Header';
+
+import { Hexagon } from './components/Hexagon';
+
+import './global.css';
+
+import styles from './App.module.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <Header />
+
+      <div className={styles.content}>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className={styles.skillContainer}>
+        <div className={styles.normal}>
+          <Hexagon
+            image='https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg'
+          />
+          <Hexagon
+            image='https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg'
+          />
+        </div>
+
+        <div className={styles.right}>
+          <Hexagon
+            image='https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg'
+          />
+          <Hexagon
+            image='https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg'
+          />
+        </div>
+
+        <div className={styles.normal}>
+          <Hexagon
+            image='https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg'
+            size={75}
+          />
+          <Hexagon
+            image='https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg'
+            size={75}
+          />
+        </div>
+
+        <div className={styles.right}>
+          <Hexagon
+            image='https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg'
+          />
+          <Hexagon
+            image='https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg'
+          />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   )
 }
 
-export default App
+export default App;
